1109d677f7bcc1fdf76cd7d24b2ac4482c54cd91
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-05-05 00:36:57 +0700

    Migration done

--- database/migrations/2024_05_04_172426_create_comments_table copy.php
+++ database/migrations/2024_05_04_172426_create_comments_table copy.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->json('body')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
             $table->timestamps();
         });
     }
